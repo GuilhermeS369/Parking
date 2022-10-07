7204112deb58e173ca8171d8a03f27dfa3e7aa43
Guilherme Santos <guilherme460@yahoo.com.br>
2022-10-06 23:00:48 -0300

    Interface Repository + atualização de serviços

---
+++ src/main/java/com/bootcamp/repository/ParkingRepository.java
@@ -0,0 +1,10 @@
+package com.bootcamp.repository;
+
+import com.bootcamp.model.Parking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingRepository extends JpaRepository<Parking, String> {
+
+}
--- src/main/java/com/bootcamp/service/ParkingService.java
+++ src/main/java/com/bootcamp/service/ParkingService.java
@@ -2,6 +2,7 @@ package com.bootcamp.service;
 
 import com.bootcamp.exception.ParkingNotFoundException;
 import com.bootcamp.model.Parking;
+import com.bootcamp.repository.ParkingRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -10,67 +11,54 @@ import java.util.*;
 @Service
 public class ParkingService {
 
-    private final static Map<String, Parking> parkingMap = new HashMap();
+    private final ParkingRepository parkingRepository;
 
-    static{
-        var id = getUUID();
-        var id2 = getUUID();
-        var id3 = getUUID();
-        var id4 = getUUID();
-        Parking parking = new Parking(id, "CKU-8318", "SP", "DK150", "RED");
-        Parking parking2 = new Parking(id2, "ACS-7949", "SP", "CIVIC", "BLACK");
-        Parking parking3 = new Parking(id3, "DAQ-6541", "SP", "CAMARO", "YELLOW");
-        Parking parking4 = new Parking(id4, "FET-7841", "SP", "MUSTANG", "WHITE");
-        parkingMap.put(id, parking);
-        parkingMap.put(id2, parking2);
-        parkingMap.put(id3, parking3);
-        parkingMap.put(id4, parking4);
 
+        public ParkingService(ParkingRepository parkingRepository) {
+        this.parkingRepository = parkingRepository;
     }
 
     public void delete(String id){
-        findById(id);
-        parkingMap.remove(id);
+            findById(id);
+        parkingRepository.deleteById(id);
     }
 
     public Parking findById(String id){
-        Parking parking = parkingMap.get(id);
-        if(parking == null) {
-            throw new ParkingNotFoundException(id);
-        }
-        return parking;
+        return parkingRepository.findById(id).orElseThrow(()->
+                new ParkingNotFoundException(id));
+
     }
 
     public List<Parking> findAll(){
-        return new ArrayList<>(parkingMap.values());
+        return new ArrayList<>(parkingRepository.findAll());
     }
+
     public Parking create(Parking parkingCreate) {
         parkingCreate.setId(getUUID());
         parkingCreate.setEntryDate(LocalDateTime.now());
-        parkingMap.put(parkingCreate.getId(), parkingCreate);
+        parkingRepository.save(parkingCreate);
         return parkingCreate;
 
     }
+    public Parking update(String id, Parking toParking) {
+        Parking parking = findById(id);
+        parking.setColor(toParking.getColor());
+        parkingRepository.save(parking);
+        return toParking;
 
+    }
+
+    public Parking exit(String id) {
+        Parking parking = findById(id);
+        parking.setExitDate(LocalDateTime.now());
+        parking.setBill(BillService.calcular(parking.getEntryDate(), parking.getExitDate()));
+        parkingRepository.save(parking);
+        return parking;
+    }
 
     private static String getUUID() {
         return UUID.randomUUID().toString().replace("-", "");
     }
 
 
-    public Parking update(String id, Parking toParking) {
-    Parking parking = findById(id);
-    parking.setColor(toParking.getColor());
-    parkingMap.replace(id, parking);
-    return toParking;
-
-    }
-
-    public Parking exit(String id) {
-    Parking parking = findById(id);
-    parking.setExitDate(LocalDateTime.now());
-    parking.setBill(BillService.calcular(parking.getEntryDate(), parking.getExitDate()));
-    parkingMap.put(id,parking);
-    return parking;
-    }
 }
